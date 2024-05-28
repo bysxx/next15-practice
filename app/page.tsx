d40ed13f37777e6d71ca742de2013f9b0681aa27
bysxx <0415white@gmail.com>
2024-05-28 14:53:25 +0900

    feat: apply next 15 features

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import { unstable_after as after } from 'next/server';
+
 import Image from "next/image";
 
 export default function Home() {
+  after(() => {
+    console.log('Page rendered');
+  })
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
